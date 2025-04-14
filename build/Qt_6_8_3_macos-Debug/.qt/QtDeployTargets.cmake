b7feb9d31cfe4d67761d5805977f34bc7ae8fa14
류예지 <yj@lyuyejiui-MacBookPro.local>
2025-04-14 10:11:27 +0900

    clear git cached

--- build/Qt_6_8_3_macos-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_veda_qt_FILE /Users/yj/veda_qt/build/Qt_6_8_3_macos-Debug/veda_qt.app/Contents/MacOS/veda_qt)
-set(__QT_DEPLOY_TARGET_veda_qt_TYPE EXECUTABLE)
--- build/Qt_6_8_3_macos-Debug/CMakeFiles/veda_qt.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for veda_qt.
-# This may be replaced when dependencies are built.
