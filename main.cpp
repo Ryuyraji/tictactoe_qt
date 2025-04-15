cc99290eebb8882527d9e3815a3813f029fd88a8
류예지 <yj@lyuyejiui-MacBookPro.local>
2025-04-15 09:44:44 +0900

    Login & Create an account features added

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-#include "mygame.h"
-
+#include "login.h"
+#include "account.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MyGame w;
+    login w;
+    //account v;
     w.show();
+    //v.show();
     return a.exec();
 }
